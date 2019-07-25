17c02922d9c1f3a342c898063aa5c0d0147df717
Ben Beauregard <bot190@gmail.com>
2019-07-24 20:45:24 -0400

    Update configs, use submodules
    Replace bash script for downloading modules with usage of git submodules

---
+++ files/bash/.config/bashrc.d/general.bash
@@ -0,0 +1,5 @@
+#History related config
+export HISTCONTROL=ignoredups:erasedups
+shopt -s histappend
+export HISTTIMEFORMAT="%s "
+source $HOME/.config/bashrc.d/dbhist/dbhist.sh
--- files/bash/.config/rc.d/general.sh
+++ files/bash/.config/rc.d/general.sh
@@ -1,7 +1,13 @@
-# general aliases
-alias ls='ls --color'
-alias l='ls -lah'
-alias tmux="tmux -2"
+# General Aliases and functions
+
+## Function definitions, these may be used in aliases so they come first
+function runIfExists {
+    if command -v $1 >/dev/null 2>&1; then
+        echo "$1"
+    else
+        echo "$2"
+    fi
+}
 
 function dus {
     if [ -n "$1" ]; then
@@ -16,4 +22,13 @@ mkcdir () {
         cd -P -- "$1"
 }
 
+# Aliases
+
+## Use exa if its available, fallback to LS otherwise
+which_ls=`runIfExists exa "ls --color"`
+alias ls="$which_ls"
+alias l='ls -lah'
+
+
+alias tmux="tmux -2"
 
