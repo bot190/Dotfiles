73ae3159c328755e95eceac81f65be111d2b77fe
Ben Beauregard <bot190@gmail.com>
2025-01-21 10:11:10 -0500

    feat(bash): add git-continue to bash aliases
    Adds git-continue aliases to bash shell.
    
    Signed-off-by: Ben Beauregard <bot190@gmail.com>

--- dot_config/bashrc.d/git.bash
+++ dot_config/bashrc.d/git.bash
@@ -9,3 +9,29 @@ function gl {
         git --no-pager log -n 2
     fi
 }
+
+
+_git_continue ()
+{
+    repo_path=$(git rev-parse --git-dir)
+
+    if [ $? -ne 0 ]; then
+        exit $?
+    fi
+
+    if [ -d "${repo_path}/rebase-merge" ]; then
+        git rebase --continue
+    elif [ -d "${repo_path}/rebase-apply" ]; then
+        git rebase --continue
+    elif [ -f "${repo_path}/MERGE_HEAD" ]; then
+        git merge --continue
+    elif [ -f "${repo_path}/CHERRY_PICK_HEAD" ]; then
+        git cherry-pick --continue
+    elif [ -f "${repo_path}/REVERT_HEAD" ]; then
+        git revert --continue
+    else
+        echo "No something in progress?"
+    fi
+}
+
+alias git-continue='_git_continue'
